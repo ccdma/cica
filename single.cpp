b0983703c444752dac2da9e8a880e82d7cc7d057
face0u0 <me@face0u0.net>
2021-11-07 00:21:42 +0900

    push

--- single.cpp
+++ single.cpp
@@ -4,14 +4,14 @@
 #include "ica.cpp"
 
 int main(){
-	ICA::Reng reng(0);
-	const auto sample = 3;
+	ICA::Reng reng(1);
+	const auto sample = 4;
 	const auto series = 1000;
 	
 	std::vector<ICA::Vector> s(sample);
 	#pragma omp parallel for
-	for (int i=0; i<sample; i++){
-		s.at(i) = ICA::ChebytSeries(i+2, series, 0.2);
+	for (int i=0; i<sample-2; i++){
+		s.at(i) = ICA::SinSeries(1.0/((double)i+1.0), series);
 	}
 	ICA::Matrix noncenterS = ICA::VStack(s);
 	ICA::Matrix S = ICA::Centerize(noncenterS);
@@ -22,7 +22,7 @@ int main(){
 	ICA::Matrix P = ICA::SimpleCirculantP(A, result.W);
 	ICA::Matrix S2 = P.transpose() * result.Y;
 
-    const int plot_num = std::min(300, series);
+    const int plot_num = std::min(1000, series);
 	std::stringstream ss;
 	ICA::Matrix SR = S.rightCols(plot_num);
 	ICA::WriteMatrix(ss, S);
