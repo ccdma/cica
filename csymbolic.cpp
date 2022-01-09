8995c7fcf80397cc88622490a97a5d31af0af766
face0u0 <me@face0u0.net>
2022-01-09 12:10:57 +0900

    push

--- csymbolic.cpp
+++ csymbolic.cpp
@@ -51,7 +51,7 @@ test_report test(const int signals, const int samplings, const int seed, const d
 }
 
 int main(){
-	const auto trials = 10;
+	const auto trials = 1000;
 	const auto sep = "\t";
 	std::cout << "commit" << sep << COMMIT_ID << std::endl;
 	std::cout << "trials" << sep << trials << std::endl;
@@ -67,8 +67,8 @@ int main(){
 	const auto samplings = 1000;
 	// const auto signals = 100;
 	// const auto stddev = 0.0;
-	std::vector<double> v1{0.02, 0.05}; // v1{10, 20, 30}
-	std::vector<int> v2 = cica::util::range(2, 150);
+	std::vector<double> v1{0.05}; // v1{10, 20, 30}
+	std::vector<int> v2 = cica::util::range(33, 150);
 	for(const auto& stddev : v1){
 	for(const auto& j : v2){
 		const auto signals = j;
