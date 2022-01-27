13a69165ed0d1f81f92705e40516b81a68fc5efe
face0u0 <me@face0u0.net>
2022-01-27 19:29:34 +0900

    push

--- csymbolic.cpp
+++ csymbolic.cpp
@@ -55,7 +55,7 @@ test_report test(const int signals, const int samplings, const int seed, const d
 }
 
 int main(){
-	const auto trials = 1000;
+	const auto trials = 100;
 	const auto sep = "\t";
 	auto timer = new cica::util::timer();
 	std::cout << "commit" << ":" << COMMIT_ID << std::endl;
@@ -71,7 +71,7 @@ int main(){
 	// const auto samplings = 1000;
 	// const auto signals = 100;
 	const auto stddev = 0.0402;
-	std::vector<int> v1 = cica::util::range(10000, 10000, 1000); // v1{10, 20, 30}
+	std::vector<int> v1 = cica::util::range(10000, 30000, 10000); // v1{10, 20, 30}
 	std::vector<int> v2 = cica::util::range(10, 500);
 	for(const auto& samplings : v1){
 	for(const auto& j : v2){
